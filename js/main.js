2c10e0f4442d44a8c7a8c766d93b494023bb6e0b
Troughie <nguyentienngoc7166@gmail.com>
2025-03-15 15:09:35 +0700

    fix something

--- js/main.js
+++ js/main.js
@@ -2,77 +2,84 @@
 let data = [];
 let a = 1;
 let cartNum = 0;
-let numPage = 16; 
+let numPage = 16;
 
 $(document).ready(function () {
-  $("#main").load("home.html")
+  $.getJSON("items.json", function (items) {
+    data = items;
+  });
 
+  function loadHomePage() {
+    $("#main").load("home.html", function () {
+      $(".collapse").collapse("hide"); // Ẩn menu nếu có
+      document.querySelectorAll(".popular-type").forEach((element) => {
+        element.addEventListener("click", function (e) {
+          e.preventDefault();
+          const type = this.getAttribute("data-type");
+          goProduct(type);
+        });
+      });
+    });
+  }
+  loadHomePage();
 
   $("#product").click(function (e) {
     e.preventDefault();
 
-    $("#main").load("product.html");
-
-    $('.collapse').collapse('hide');
-    $.getJSON("items.json", function (items) {
-      data = items;
-      showImage(data)
-      popupImage(data)
-    });
-  })
+    goProduct(e);
+  });
 
   $("#home").click(function (e) {
     e.preventDefault();
-    $("#main").load("home.html");
-    $('.collapse').collapse('hide');
-  })
+    loadHomePage();
+  });
 
   $("#home-logo").click(function (e) {
     e.preventDefault();
-    $("#main").load("home.html");
-    $('.collapse').collapse('hide');
-  })
+    loadHomePage();
+  });
 
   $("#gallery").click(function (e) {
     e.preventDefault();
-    $("#main").load("gallery.html");
-    $('.collapse').collapse('hide');
-  })
+    $("#main").load("gallery.html", scrollToTop());
+    $(".collapse").collapse("hide");
+  });
 
   $("#blog").click(function (e) {
     e.preventDefault();
-    $("#main").load("blog.html");
-    $('.collapse').collapse('hide');
-  })
+    $("#main").load("blog.html", () => {
+      window.scrollTo(0, 0);
+    });
+    $(".collapse").collapse("hide");
+  });
 
   $("#about").click(function (e) {
     e.preventDefault();
-    $("#main").load("aboutus.html");
-    $('.collapse').collapse('hide');
-  })
+    $("#main").load("aboutus.html", scrollToTop());
+    $(".collapse").collapse("hide");
+  });
 
   $("#login").click(function (e) {
     e.preventDefault();
     $("#main").load("signin.html");
-  })
+  });
 
   $("#register").click(function (e) {
     e.preventDefault();
     $("#main").load("signup.html");
-  })
+  });
 
   $("#faq").click(function (e) {
     e.preventDefault();
     $("#main").load("faq.html");
-    $('.collapse').collapse('hide');
-  })
+    $(".collapse").collapse("hide");
+  });
 
   $("#contact").click(function (e) {
     e.preventDefault();
     $("#main").load("contactus.html");
-    $('.collapse').collapse('hide');
-  })
-
+    $(".collapse").collapse("hide");
+  });
 
   $(document).on("click", ".popup", function () {
     let id = $(this).data("id");
@@ -81,53 +88,82 @@ $(document).ready(function () {
 
     popupImage(product[0]);
     $("#popupImage").modal("show");
-  })
+  });
+});
 
-})
+const scrollToTop = () => {
+  window.scrollTo(0, 0);
+};
+const goProduct = (type) => {
+  $("#main").load("product.html", () => {
+    window.scrollTo(0, 0);
+    show(type);
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    const btn_count = document.getElementsByClassName("btn-count");
+    if (cart.length > 0) {
+      btn_count[0].classList.add("show");
+      btn_count[0].innerText = cart.length;
+    }
+
+    console.log(cart.length);
+  });
+
+  $(".collapse").collapse("hide");
+  $.getJSON("items.json", function () {
+    showImage(data);
+    popupImage(data);
+  });
+  window.scrollY = 0;
+};
 
 function loadBlog() {
-  $("#main").load("blog.html")
+  $("#main").load("blog.html");
 }
 function loadLogin() {
-  $("#main").load("signin.html")
+  $("#main").load("signin.html");
 }
 function loadFaq() {
-  $("#main").load("faq.html")
+  $("#main").load("faq.html");
 }
 
-
-	
-
-
-// Product items 
+// Product items
 
 function showImage(data) {
   let length = data.length;
-  let s = []
+  let s = [];
 
   $.each(data, function (i, item) {
     s.push(`
               <div class="list-item ${item.brand}" data-brand="${item.brand}" >
                   <div class="list-item__img">
-                    <img height="260px" class="popup" src="${item.img}" data-id="${item.id}">
-                    <button id="add-cart" onclick="addCart(${item.id})"><i class="fa-solid fa-cart-plus"></i>Add to cart</button>
+                  <div class="item-top">
+                  <img height="260px" class="img-product" src="${
+                    item.img
+                  }" data-id="${item.id}">
+                  <button id="add-cart" onclick="addCart(${
+                    item.id - 1
+                  })"><i class="fa-solid fa-cart-plus"></i>Add to cart</button>
+                  </div>
                     </div>
-                    <div class="list-item__info">
-                    <p class="item-name">${item.name}</p>
-                    <p class="item-brand">${item.brand}</p>
-                    <p class="item-info">${item.color}</p>
-                    <p class="item-info">${item.price}</p>
-                    </div>
+                    <div >
+                      <div class="item-header">
+                        <p class="item-name">${item.name}</p>
+                        <p class="item-brand">${item.brand}</p>
+                      </div>
+                      <div class="list-item__info">
+                      <p class="item-info">${item.color}</p>
+                      <p class="item-info">${item.price}</p>
+                      </div>
+                  </div>
                 </div>
-  `)
-    $("#data-product").html(s.join(''))
+  `);
+    $("#data-product").html(s.join(""));
   });
 }
 
 // Popup item in product
 
 function popupImage(item) {
-
   let s = `
               <div class="popup-item">
                     <img width="480px" height="480px" src="${item.img}">
@@ -139,57 +175,84 @@ function popupImage(item) {
                           <p>Wing:<span>${item.Wing}</span></p>
                           <p>Length:<span>${item.Length}</span></p>
                           <p>Warranty:<span>${item.Warranty}</span></p>
+                          <button class="add-cart-popup" onclick="addCart(${item.id})"><i class="fa-solid fa-cart-plus"></i>Add to cart</button>
+                    </div>
                         </div>
                       </div>
                     
                 </div>
   `;
-  $("#modal-item").html(s)
+  $("#modal-item").html(s);
 }
 
 // filter item in product
-function show() {
-  let brands = $(".brand:checked").map(function () { return $(this).val() }).toArray().toString();
-  let categories = $(".category:checked").map(function () { return $(this).val() }).toArray().toString();
-  let colors = $(".color:checked").map(function () { return $(this).val() }).toArray().toString();
-  let type = $(".type:checked").map(function () { return $(this).val() }).toArray().toString();
+function show(typeClick) {
+  let brands = $(".brand:checked")
+    .map(function () {
+      return $(this).val();
+    })
+    .toArray()
+    .toString();
+  let categories = $(".category:checked")
+    .map(function () {
+      return $(this).val();
+    })
+    .toArray()
+    .toString();
+  let colors = $(".color:checked")
+    .map(function () {
+      return $(this).val();
+    })
+    .toArray()
+    .toString();
+
+  $(".type").each(function () {
+    if (typeClick === $(this).val()) {
+      $(this).prop("checked", true);
+    }
+  });
+
+  let type = $(".type:checked")
+    .map(function () {
+      return $(this).val();
+    })
+    .toArray()
+    .toString();
+
   let newData = null;
 
   if (brands.length == 0) {
     newData = data;
-  }
-
-  else {
-    newData = data.filter(item => brands.search(item.brand) >= 0);
+  } else {
+    newData = data.filter((item) => brands.search(item.brand) >= 0);
   }
 
   if (categories.length > 0) {
-    newData = data.filter(item => categories.search(item.category) >= 0);
-
+    newData = data.filter((item) => categories.search(item.category) >= 0);
   }
 
   if (colors.length > 0) {
-    newData = data.filter(item => colors.search(item.color) >= 0);
+    newData = data.filter((item) => colors.search(item.color) >= 0);
   }
 
-  if(type.length > 0) {
-    newData = data.filter(item => type.search(item.type) >= 0);
+  if (type.length > 0) {
+    newData = data.filter((item) => type.search(item.type) >= 0);
   }
 
-  if(type.length > 0 & colors.length > 0) {
-    newData = data.filter(item => type.search(item.type) >= 0);
-    newData = newData.filter(item => colors.search(item.color) >= 0)
-  }
-  
-  if (type.length > 0 & colors.length > 0 & brands.length >0) {
-    newData = data.filter(item => type.search(item.type) >= 0);
-    newData = newData.filter(item => colors.search(item.color) >= 0)
-    newData = newData.filter(item => brands.search(item.brand) >= 0);
+  if ((type.length > 0) & (colors.length > 0)) {
+    newData = data.filter((item) => type.search(item.type) >= 0);
+    newData = newData.filter((item) => colors.search(item.color) >= 0);
   }
 
-  if(type.length > 0 & brands.length >0) {
-    newData = data.filter(item => type.search(item.type) >= 0);
-    newData = newData.filter(item => brands.search(item.brand) >= 0);
+  if ((type.length > 0) & (colors.length > 0) & (brands.length > 0)) {
+    newData = data.filter((item) => type.search(item.type) >= 0);
+    newData = newData.filter((item) => colors.search(item.color) >= 0);
+    newData = newData.filter((item) => brands.search(item.brand) >= 0);
+  }
+
+  if ((type.length > 0) & (brands.length > 0)) {
+    newData = data.filter((item) => type.search(item.type) >= 0);
+    newData = newData.filter((item) => brands.search(item.brand) >= 0);
   }
 
   showImage(newData);
@@ -197,65 +260,64 @@ function show() {
 
 // show and hidden filter
 function showFilter() {
-  a++
-  if (a % 2 == 0) {
-    document.querySelector(".filter-zoom").classList.add("show")
-  }
-  else {
-    document.querySelector(".filter-zoom").classList.remove("show")
-  }
+  document.querySelector(".filter-zoom").classList.toggle("show");
+  document.querySelector(".filter-nav").classList.toggle("active-filter");
 }
 
-
 function openCart() {
-  document.querySelector("#cart").classList.add("show-cart")
-  var removeItemCartBtn = document.getElementsByClassName('cart-remove')
-
+  document.querySelector("#cart").classList.add("show-cart");
+  fillItemToCart();
+  var removeItemCartBtn = document.getElementsByClassName("cart-remove");
 
   for (var index = 0; index <= removeItemCartBtn.length; index++) {
+    console.log(removeItemCartBtn[0]);
     var button = removeItemCartBtn[index];
-    button.addEventListener('click', removeCartItem)
+    button?.addEventListener("click", removeCartItem);
   }
   updateCartTotal();
 }
 
 function closeCart() {
-  document.querySelector("#cart").classList.remove("show-cart")
+  document.querySelector("#cart").classList.remove("show-cart");
 }
 
+function fillItemToCart() {
+  const cart = JSON.parse(localStorage.getItem("cart"));
+  var s = [];
+  $.each(cart, function (i, item) {
+    s.push(`
+        <div class="cart-body">
+          <span>${item.name}</span>
+          <span class="cart-price">${item.price}</span>
+          <div>
+          <input class="cart-qty" style="width: 50px" type="number" value="${item.qty}">
+          <button style="width:50px; padding:0; margin-bottom:5px" class="btn btn-danger cart-remove">X</button>
+          </div>
+          </div>
+       `);
+    $("#menuCart").html(s.join(" "));
+  });
+  return cart;
+}
 
 function addCart(id) {
-
-  var item = data[id]
+  console.log(id);
+  var item = data[id];
   var newEle = {
-    "id": id,
-    "name": item.name,
-    "price": item.price,
-    "qty": 1
-  }
+    id: id,
+    name: item.name,
+    price: item.price,
+    qty: 1,
+  };
 
   if (localStorage.getItem("cart") == null) {
     cart = [];
-  }
-  else {
-      cart = JSON.parse(localStorage.getItem("cart"));
-      var s = [];
-      $.each(cart, function (i, item) {
-        s.push(`
-          <div class="cart-body">
-            <span>${item.name}</span>
-            <span class="cart-price">${item.price}</span>
-            <div>
-            <input class="cart-qty" style="width: 50px" type="number" value="${item.qty}">
-            <button style="width:50px; padding:0; margin-bottom:5px" class="btn btn-danger cart-remove">X</button>
-            </div>
-            </div>
-         `)
-      $("#menuCart").html(s.join(" "))
-    })
+  } else {
+    cart = fillItemToCart();
   }
   var find = false;
-  cart.forEach(element => {
+
+  cart.forEach((element) => {
     if (element.id == id) {
       element.qty++;
 
@@ -269,89 +331,121 @@ function addCart(id) {
   localStorage.setItem("cart", JSON.stringify(cart));
   alert("add cart succeeded !");
   updateCartTotal();
-  
-  
-  var removeItemCartBtn = document.getElementsByClassName('cart-remove')
-  for (var index = 0; index <= removeItemCartBtn.length; index++) {
-    var button = removeItemCartBtn[index];
-    button.addEventListener('click', removeCartItem)
-  }
-
 }
 
-
-
 function removeCartItem(event, id) {
-  var btnClicked = event.target
-  btnClicked.parentElement.parentElement.remove()
+  var btnClicked = event.target;
+  btnClicked.parentElement.parentElement.remove();
 
+  let cart = JSON.parse(localStorage.getItem("cart"));
 
-  let cart = JSON.parse(localStorage.getItem('cart'))
-
-  let updateProducts = cart.filter(item => item.id == id)
+  let updateProducts = cart.filter((item) => item.id == id);
   console.log(updateProducts);
-  localStorage.setItem('cart', JSON.stringify(updateProducts))
+  localStorage.setItem("cart", JSON.stringify(updateProducts));
   updateCartTotal();
 }
 
 function updateCartTotal() {
-  var cartItemContainer = document.getElementById('menuCart')
-  var cartItems = cartItemContainer.getElementsByClassName("cart-body")
-  cartItems.length += 1
-  var total = 0
+  var cartItemContainer = document.getElementById("menuCart");
+  var cartItems = cartItemContainer.getElementsByClassName("cart-body");
+  cartItems.length += 1;
+  var total = 0;
   for (let index = 0; index < cartItems.length; index++) {
     const cartItem = cartItems[index];
-    var priceElement = cartItem.getElementsByClassName("cart-price")[0]
-    var qtyElement = cartItem.getElementsByClassName('cart-qty')[0]
+    var priceElement = cartItem.getElementsByClassName("cart-price")[0];
+    var qtyElement = cartItem.getElementsByClassName("cart-qty")[0];
 
-    var price = priceElement.innerText.replace('$', '')
-    var qty = qtyElement.value
+    var price = priceElement.innerText.replace("$", "");
+    var qty = qtyElement.value;
 
-    total = total + price * qty
+    total = total + price * qty;
   }
 
-  document.getElementsByClassName('cart-total')[0].innerText = '$' + total
-  document.getElementsByClassName('btn-count')[0].innerText = cartItems.length
+  document.getElementsByClassName("cart-total")[0].innerText = "$" + total;
+  document.getElementsByClassName("btn-count")[0].innerText = cartItems.length;
 }
 
-
 function signUp() {
-  var mail = $("#email").val()
-  var username = $("#username").val()
-  var pass = $("#password").val()
-
-
+  var mail = $("#email").val();
+  var username = $("#username").val();
+  var pass = $("#password").val();
 
   var user = {
     email: mail,
     username: username,
-    password: pass
-  }
-  var json = JSON.stringify(user)
-  localStorage.setItem('username', json)
+    password: pass,
+  };
+  var json = JSON.stringify(user);
+  localStorage.setItem("username", json);
   let info = [];
   info.push(`
     successful your account
     name: ${username},
     email: ${mail},
     password: ${pass}
-  `)
+  `);
 
-  var infos = info.join('\n')
-  alert(infos)
-  $("#main").load("signin.html")
+  var infos = info.join("\n");
+  alert(infos);
+  $("#main").load("signin.html");
 }
 
-
-
 function signIn(e) {
-  var user = JSON.parse.localStorage.getItem('username')
-  var data = JSON.parse(user)
+  var user = JSON.parse.localStorage.getItem("username");
+  var data = JSON.parse(user);
   console.log(user);
 }
 
+let debounceTimer;
+document.getElementById("search-item").addEventListener("input", function () {
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(() => {
+    const search = document.getElementById("item-list");
 
+    const searchValue = this.value.trim().toLowerCase();
 
+    if (searchValue === "") {
+      search.classList.remove("show");
+      return;
+    }
 
+    search.classList.add("show");
 
+    const result = data.filter((item) =>
+      item.name.toLowerCase().includes(searchValue)
+    );
 
+    var s = [];
+    if (result.length === 0) {
+      s.push(`
+        <div class="search-body">
+          <span>No matching products</span>
+        </div>
+      `);
+    } else {
+      $.each(result, function (i, item) {
+        s.push(`
+          <div class="search-body">
+            <span>${item.name}</span>
+          </div>
+        `);
+      });
+    }
+
+    $("#item-list").html(s.join(" "));
+  }, 300);
+});
+
+document
+  .querySelector(".navbar-nav")
+  .addEventListener("click", function (event) {
+    if (event.target.tagName === "A") {
+      event.preventDefault();
+
+      document
+        .querySelectorAll(".navbar-nav a")
+        .forEach((link) => link.classList.remove("active"));
+
+      event.target.classList.add("active");
+    }
+  });
